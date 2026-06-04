5243e2ece0241bda3ff78149f0b893ed608920a2
John Klein <johnklein.dev@gmail.com>
2026-06-04 16:50:38 +0200

    added custom theme

--- CompanionApp/app/src/main/java/de/luh/hci/pclab/MainActivity.kt
+++ CompanionApp/app/src/main/java/de/luh/hci/pclab/MainActivity.kt
@@ -5,14 +5,16 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import de.luh.hci.pclab.navigation.ui.Navigation
-import de.luh.hci.pclab.ui.theme.PhysicalComputingTheme
+import de.luh.hci.pclab.ui.theme.AppTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            PhysicalComputingTheme {
+            AppTheme(
+                dynamicColor = false,
+            ) {
                 Navigation()
             }
         }
